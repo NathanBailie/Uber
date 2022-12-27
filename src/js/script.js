98e2fe079fd4bb0e16b253b2f8a9f1a7432bf1a0
Nathan <nathanbailie.dev@gmail.com>
2022-12-27 12:19:17 +0300

    saved to repository my project from the local computer

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// Hamburger
+let hamburger = document.querySelector('.hamburger');
+let links = document.querySelector('.menu__links');
+
+for (let link of links.children) {
+	link.addEventListener('click', () => {
+		links.classList.remove('menu__links_active');
+		hamburger.classList.remove('hamburger_active')
+	})
+}
+
+hamburger.addEventListener('click', function () {
+	links.classList.toggle('menu__links_active');
+	hamburger.classList.toggle('hamburger_active')
+})
+
+
+// Scrolling
+let upChevron = document.querySelector('.up-chevron');
+let clientHeight = document.documentElement.clientHeight;
+
+upChevron.addEventListener('click', function () {
+	window.scrollTo({
+		top: 0,
+		behavior: "instant"
+	});
+
+})
+
+window.addEventListener('scroll', function () {
+	if (document.documentElement.scrollTop > clientHeight) {
+		upChevron.style.display = 'block';
+	} else {
+		upChevron.style.display = 'none';
+
+	}
+});
